7fbb9697dfe5385593a8464676a65d99006a5a18
jaya chauhan <chauhanjaya01@gmail.com>
2023-12-15 18:42:39 +0530

    worked on professor landing page

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,24 @@
 
+const video = document.getElementById("video");
+const circlePlayButton = document.getElementById("circle-play-b");
+
+function togglePlay() {
+	if (video.paused || video.ended) {
+		video.play();
+	} else {
+		video.pause();
+	}
+}
+
+circlePlayButton.addEventListener("click", togglePlay);
+video.addEventListener("playing", function () {
+	circlePlayButton.style.opacity = 0;
+});
+video.addEventListener("pause", function () {
+	circlePlayButton.style.opacity = 1;
+});
+
+
 $(document).ready(function(){
     $(".toggle-password").click(function() {
 
@@ -21,3 +41,6 @@ $(document).ready(function(){
         }
       });
 });
+
+
+
